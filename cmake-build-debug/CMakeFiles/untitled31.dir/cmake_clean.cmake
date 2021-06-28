91551d307549b45a6afdce31e8756aa9e922c938
natoliveira <nataliadeolive@gmail.com>
2021-06-28 08:25:20 -0300

    algoritmo problemaTroco implementado

---
+++ cmake-build-debug/CMakeFiles/untitled31.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled31.dir/troco.cpp.o"
+  "untitled31"
+  "untitled31.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
